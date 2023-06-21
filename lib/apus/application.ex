0dfca30f9ad657d0eaf986eee9a4e8db737c41ea
Brian Nilsen <bnilsen@papa.com>
2023-06-21 14:41:48 -0700

    Sometimes Elixir syntax just mystifies me, but it's okay, because the expert I was following made a similar mistake and then blamed everyone else for not reading the docs

--- lib/apus/application.ex
+++ lib/apus/application.ex
@@ -7,7 +7,7 @@ defmodule Apus.Application do
     children = [
       %{id: Apus.SentMessages, start: {Apus.SentMessages, :start_link, []}},
       {Task.Supervisor, name: Apus.TaskSupervisorStrategy.supervisor_name()},
-      {Redix, name: :apus_redix, url: Application.get_env(:apus, :url, "localhost:6379")}
+      {Redix, {Application.get_env(:apus, :url, "redis://localhost:6379"), [name: :apus_redix]}}
     ]
 
     opts = [strategy: :one_for_one, name: Apus.Supervisor]
