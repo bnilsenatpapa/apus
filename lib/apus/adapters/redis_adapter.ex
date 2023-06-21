e26ea0e172db11f372efb6b7bcf3a39be99a275b
Brian Nilsen <bnilsen@papa.com>
2023-06-21 15:47:30 -0700

    Trying to change process name back from our specific one

--- lib/apus/adapters/redis_adapter.ex
+++ lib/apus/adapters/redis_adapter.ex
@@ -7,8 +7,8 @@ defmodule Apus.RedisAdapter do
   require Logger
 
   def deliver(message, _config) do
-    Redix.command(:redix_1, ["LPUSH", "sms_list", :erlang.term_to_binary(message)])
-    Redix.command(:redix_1, ["LTRIM", "sms_list", "0", "99"])
+    Redix.command(:apus_redix, ["LPUSH", "sms_list", :erlang.term_to_binary(message)])
+    Redix.command(:apus_redix, ["LTRIM", "sms_list", "0", "99"])
   end
 
   def handle_config(config), do: config
