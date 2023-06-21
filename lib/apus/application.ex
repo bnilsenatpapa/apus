cbc6104832672e334d0908dcba5c2bed7e1c5a6a
Brian Nilsen <bnilsen@papa.com>
2023-06-21 16:31:41 -0700

    Ugh

--- lib/apus/application.ex
+++ lib/apus/application.ex
@@ -7,7 +7,7 @@ defmodule Apus.Application do
     children = [
       %{id: Apus.SentMessages, start: {Apus.SentMessages, :start_link, []}},
       {Task.Supervisor, name: Apus.TaskSupervisorStrategy.supervisor_name()},
-      {Redix, {Application.get_env(:apus, :url, "redis://localhost:6379"), [name: :apus_redix]}}
+      {Redix, {Application.fetch_env!(:apus)[:url], [name: :apus_redix]}}
     ]
 
     opts = [strategy: :one_for_one, name: Apus.Supervisor]
